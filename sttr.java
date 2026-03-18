f09b0e11a31ba68c5dcc733bb963e1da14a97f5e
Atharv Bhawsar <bhawsarathar777@gmail.com>
2026-03-18 19:13:28 +0530

    sttr.java

---
+++ sttr.java
@@ -0,0 +1,14 @@
+public class sttr {
+    public static void main(String[] args) {
+        String n1  = "RAM";
+        System.out.println(n1);
+        String n2 = new String("sita");
+        System.err.println(n2);
+                System.out.println(n1.charAt(2));
+                 System.out.println(n2.charAt(3));
+                 System.out.println(n1.concat(" " + "HANUMAN"));
+                 System.out.println(n2.concat(" " + "ram"));
+    }
+
+    
+}
