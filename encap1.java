a4d94b1feea73b757f8c284bb8751cd3daba7a48
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-03-19 15:58:14 +0530

    encapsulation second code

---
+++ encap1.java
@@ -0,0 +1,34 @@
+
+    class Human 
+{
+    private int age;
+    private String su;
+     public int getage() 
+    {
+        return age; 
+    }
+     public String getname()
+    {
+        return su ;
+    } 
+     public void setage(int age)
+     {
+        this.age = age ;
+     }
+     public void setname (String su)
+     {
+        this.su = su;
+     }
+
+}
+
+public class encap1 {
+    public static void main(String[] args) 
+    {  Human obj = new Human();
+        obj.setage(12); 
+        obj.setname("ram");
+        System.out.println(obj.getname() +" = "+ obj.getage());
+    }
+}
+
+
