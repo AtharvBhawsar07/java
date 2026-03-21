ee9a152f81a521573854e4d889a82ed074d9c592
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-03-21 11:36:51 +0530

    final part of inheritance(optinal)

---
+++ fin.java
@@ -0,0 +1,17 @@
+public class fin {
+
+    public static void main(String[] args) 
+    {
+        Ad a = new Ad();
+        int r =a.add(23 , 89);
+        int r1= a.sub(57, 23);
+        int r2 = a.mul(8,19);
+        int r3 = a.div(72,9);
+       System.out.println(r +"  "+ r2 +"  " +"  "+ r3+"  "+ r1);
+
+}
+    
+
+
+    
+}
