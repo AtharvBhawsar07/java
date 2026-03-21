bfb5d32e5e49cff7d5965532522a91358df47490
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-03-21 16:32:29 +0530

    Multi level inheritance

--- fin.java
+++ fin.java
@@ -2,12 +2,13 @@ public class fin {
 
     public static void main(String[] args) 
     {
-        Ad a = new Ad();
+        advan a = new advan();
         int r =a.add(27 , 89);
         int r1= a.sub(57, 23);
         int r2 = a.mul(8,19);
         int r3 = a.div(72,9);
-       System.out.println(r +"  "+ r2 +"  " +"  "+ r3+"  "+ r1);
+        double r4 = a.power(5,4);
+       System.out.println(r +"  "+ r2 +"  " +"  "+ r3+"  "+ r1+"  "+ r4);
 
 }
     
