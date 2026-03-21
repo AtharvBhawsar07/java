44ed1b340875baa18d7fd463404707f8172a6bf0
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-03-21 11:32:21 +0530

    part one of inheritance

---
+++ cal.java
@@ -0,0 +1,31 @@
+class Cl
+{
+
+    public int add(int a , int b)
+{     
+        return  a+b;
+}
+    public int sub(int a , int b)
+{ 
+    return   a - b;
+
+}
+
+
+}
+
+public class cal
+{
+    public static void main(String[] args) 
+    {
+        Cl c = new Cl();
+        int r1 =c.add(23 , 89);
+        int r2= c.sub(57, 23);
+        
+       
+       //System.out.println(r +"  "+ r2 +"  " +"  "+ r3+"  "+ r1);
+    }
+
+}
+    
+
