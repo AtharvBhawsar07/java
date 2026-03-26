a9a12503d803b94a74ea2392dc49538693f22524
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-03-26 11:25:36 +0530

    Final Variable

---
+++ f1.java
@@ -0,0 +1,9 @@
+public class f1 {
+    public static void main(String[] args) 
+    {
+        final int num = 45;    // example of final variable 
+         // num = 76;
+        System.out.println(num);
+    }
+    
+}
