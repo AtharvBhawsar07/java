64f7d372d9a7428874b3ab9fa94befad6fdcf50f
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-04-09 22:56:37 +0530

    basic array

---
+++ Ar1.java
@@ -0,0 +1,10 @@
+public class Ar1 {
+    public static void main(String[] args) {
+        int[] arr = {79, 80 , 81, 82, 83};
+
+        // Traversing array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
