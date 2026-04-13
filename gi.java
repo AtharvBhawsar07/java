7e263eadd71d5b27dcdfeeca88f430390740afbe
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-04-13 23:08:26 +0530

    if else code

---
+++ gi.java
@@ -0,0 +1,11 @@
+public class gi {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("Positive Number");
+        } else {
+            System.out.println("Negative Number");
+        }
+    }
+}
