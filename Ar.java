8dc806b7bac5f8a276f9c1d61cc03bc58cb07074
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-04-20 21:19:51 +0530

    change

--- Ar.java
+++ Ar.java
@@ -2,11 +2,11 @@ class Ar
 {
     public static void main(String a[])
     {
-        int nums [][]= new int [4] [4];
-          for( int i=0; i <4; i++)
+        int nums [][]= new int [5] [5];
+          for( int i=0; i <5; i++)
               
           {
-           for(int j = 0; j<4; j++)
+           for(int j = 0; j<5; j++)
              {
                   nums[i][j]= (int)(Math.random()*10);
 
