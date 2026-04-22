cbe2f0317133ed650c7558f3ca7dbc4c83086a76
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-04-22 21:28:48 +0530

    logic

---
+++ p1.java
@@ -0,0 +1,11 @@
+public class p1 {
+    public static void main(String[] args) {
+
+        Integer a = 200;
+        Integer b = 200;
+
+        System.out.println(a == b);   // reference comparison
+        System.out.println(a.equals(b)); // value comparison
+
+    }
+}
