82d5e85b49dd9a263173ebc307f78970324a7de5
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-04-23 21:57:19 +0530

    logic

---
+++ f5.java
@@ -0,0 +1,12 @@
+public class f5
+ {
+    public static void main(String[] args) {
+
+        Integer a = 100;
+        Integer b = 100;
+
+        System.out.println(a == b);      // reference compare
+        System.out.println(a.equals(b)); // value compare
+
+    }
+}
