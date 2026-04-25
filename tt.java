4cba55823639f163f008390846580974243e1c1e
Atharv Bhawsar <bhawsaratharv777@gmail.com>
2026-04-25 19:33:45 +0530

    logic

---
+++ tt.java
@@ -0,0 +1,10 @@
+public class tt {
+    public static void main(String[] args) {
+
+        System.out.println(0.1 + 0.2 == 0.3);
+
+        // Extra: to see actual value
+        System.out.println(0.1 + 0.2);
+
+    }
+}
